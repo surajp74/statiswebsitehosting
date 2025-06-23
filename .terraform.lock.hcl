6feb2ac5552ae444a899d430ac27f0784bc51854
surajp74 <surajppatil29@gmail.com>
2025-06-23 16:35:15 +0530

    Adding s3 config and simple html files

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/aws" {
   version = "6.0.0"
   hashes = [
     "h1:DhxTTyLjzmC0wRCodYOWkIGwHAYb2Fcg3zFc/3sIpvk=",
+    "h1:UIEId3EdDvMWAZ6C3Wvh/WRtlemiPKzsn3EpEIKK+08=",
     "zh:16b1bb786719b7ebcddba3ab751b976ebf4006f7144afeebcb83f0c5f41f8eb9",
     "zh:1fbc08b817b9eaf45a2b72ccba59f4ea19e7fcf017be29f5a9552b623eccc5bc",
     "zh:304f58f3333dbe846cfbdfc2227e6ed77041ceea33b6183972f3f8ab51bd065f",
--- main.tf
+++ main.tf
@@ -1,10 +1,39 @@
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f535a71b34f2d44a"
-  instance_type = "t3.micro"
+# resource "aws_instance" "my_ec2" {
+#   ami           = "ami-0f535a71b34f2d44a"
+#   instance_type = "t3.micro"
+# }
+
+# This create s3 bucket in aws
+resource "aws_s3_bucket" "staticweb" {
+  bucket = "staticwebhoisting"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+# This will enables static website hosting
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.staticweb.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# This is used to upload files (objects) into the S3 bucket.
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.staticweb.id
+  key    = "index.html"
+  source = "website/index.html"
 }
