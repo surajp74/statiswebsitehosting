4a671a3109719feab51a770473d3b694a9de7e23
surajp74 <surajppatil29@gmail.com>
2025-06-23 13:36:28 +0530

    added info to run main file

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f535a71b34f2d44a"
+  instance_type = "t3.micro"
 }
