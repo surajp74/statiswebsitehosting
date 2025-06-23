6fb51be1e53d1ab6bc58c3366dc1b29ae06ee225
surajp74 <surajppatil29@gmail.com>
2025-06-23 22:45:44 +0530

    added tf file to create s3 bucket with proper policy and object

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 
 # This create s3 bucket in aws
 resource "aws_s3_bucket" "staticweb" {
-  bucket = "staticwebhoisting"
+  bucket = "staticwebhoistingdemo"
 
   tags = {
     Name = "My bucket"
@@ -32,8 +32,43 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 }
 
 # This is used to upload files (objects) into the S3 bucket.
-resource "aws_s3_object" "object" {
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.staticweb.id
+  key          = "index.html"
+  source       = "website/index.html"
+  content_type = "text/html"
+
+  # source_hash = filemd5("website/index.html") // filemd5() recalculates the hash of the file every time it's changed — forcing Terraform to re-upload.
+}
+
+resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.staticweb.id
-  key    = "index.html"
-  source = "website/index.html"
+  key = "error.html"
+  source = "website/error.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.staticweb.id
+
+  block_public_acls       =false
+  block_public_policy     =false
+  ignore_public_acls      =false
+  restrict_public_buckets =false
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.staticweb.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.staticweb.arn}/*"
+      }
+    ]
+  })
 }
